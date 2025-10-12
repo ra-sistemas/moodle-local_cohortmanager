829cfef2f6e890862f22d89ef264892c98503028
Davison Almeida <davison.almeida@revvo.com.br>
2025-10-12 08:54:14 -0300

    Remove strings duplicadas

--- lang/en/local_cohortmanager.php
+++ lang/en/local_cohortmanager.php
@@ -51,17 +51,7 @@ $string['nodecription'] = 'No description';
 $string['failedtoloadcohorts'] = 'Failed to load cohorts. Please try again.';
 $string['failedtodeletecohort'] = 'Failed to delete cohort. Please try again.';
 $string['failedtosavecohort'] = 'Failed to save cohort. Please try again.';
-$string['deleteconfirmation'] = 'Are you sure you want to delete this cohort?';
-$string['cohortname'] = 'Cohort Name';
-$string['entercohortname'] = 'Enter cohort name';
-$string['idnumberdescription'] = 'A unique identifier for the cohort';
-$string['enteridnumber'] = 'Enter ID number';
-$string['entercohortdescription'] = 'Enter cohort description';
-$string['cohortdescription'] = 'A detailed description of the cohort';
-$string['defaulttheme'] = 'Default Theme';
-$string['boost'] = 'Boost';
-$string['boostclean'] = 'Boost Clean';
-$string['themedescription'] = 'Select a theme for the cohort (requires allowcohortthemes to be enabled)';
+$string['deleteconfirmation'] = 'Are you sure you want to delete "%s"?';
 
 // CohortCreate component strings
 $string['basicinformation'] = 'Basic Information';
@@ -100,90 +90,6 @@ $string['username'] = 'Username';
 $string['email'] = 'Email';
 
 // CohortEdit component strings
-$string['loadingcohortdata'] = 'Loading cohort data...';
-$string['failedtoloadcohortdata'] = 'Failed to load cohort data. Please try again.';
-$string['editcohort'] = 'Edit Cohort';
-$string['failedtoupdatecohort'] = 'Failed to update cohort. Please try again.';
-$string['saving'] = 'Saving...';
-$string['savechanges'] = 'Save Changes';
-
-// App.vue strings
-$string['cohortmanager'] = 'Cohort Manager';
-$string['newcohort'] = 'New Cohort';
-$string['searchcohorts'] = 'Search cohorts...';
-$string['loadingcohorts'] = 'Loading cohorts...';
-$string['name'] = 'Name';
-$string['idnumber'] = 'ID Number';
-$string['description'] = 'Description';
-$string['visible'] = 'Visible';
-$string['hidden'] = 'Hidden';
-$string['actions'] = 'Actions';
-$string['edit'] = 'Edit';
-$string['delete'] = 'Delete';
-$string['page'] = 'Page';
-$string['of'] = 'of';
-$string['nocoortsfound'] = 'No cohorts found';
-$string['createyourfirstcohort'] = 'Create your first cohort to get started.';
-$string['createnewcohort'] = 'Create New Cohort';
-$string['cancel'] = 'Cancel';
-$string['save'] = 'Save';
-$string['nodecription'] = 'No description';
-$string['failedtoloadcohorts'] = 'Failed to load cohorts. Please try again.';
-$string['failedtodeletecohort'] = 'Failed to delete cohort. Please try again.';
-$string['failedtosavecohort'] = 'Failed to save cohort. Please try again.';
-$string['deleteconfirmation'] = 'Are you sure you want to delete "%s"?';
-
-// CohortCreate.vue strings
-$string['createnewcohort'] = 'Create New Cohort';
-$string['basicinformation'] = 'Basic Information';
-$string['cohortname'] = 'Cohort Name *';
-$string['entercohortname'] = 'Enter cohort name';
-$string['cohortnamedescription'] = 'The display name of the cohort';
-$string['idnumber'] = 'ID Number *';
-$string['enteridnumber'] = 'Enter ID number';
-$string['idnumberdescription'] = 'A unique identifier for the cohort';
-$string['description'] = 'Description';
-$string['entercohortdescription'] = 'Enter cohort description';
-$string['cohortdescription'] = 'A detailed description of the cohort';
-$string['settings'] = 'Settings';
-$string['makecohortvisible'] = 'Make the cohort visible to users';
-$string['theme'] = 'Theme';
-$string['defaulttheme'] = 'Default Theme';
-$string['boost'] = 'Boost';
-$string['boostclean'] = 'Boost Clean';
-$string['themedescription'] = 'Select a theme for the cohort (requires allowcohortthemes to be enabled)';
-$string['category'] = 'Category';
-$string['systemcategory'] = 'System Category';
-$string['coursecategorybyidnumber'] = 'Course Category by ID Number';
-$string['coursecategorybyid'] = 'Course Category by ID';
-$string['entercoursecategoryidnumber'] = 'Enter course category ID number';
-$string['entercoursecategoryid'] = 'Enter course category ID';
-$string['reset'] = 'Reset';
-$string['creating'] = 'Creating...';
-$string['createcohort'] = 'Create Cohort';
-$string['failedtocreatecohort'] = 'Failed to create cohort. Please try again.';
-$string['resetform'] = 'Reset Form';
-
-// CohortDetail.vue strings
-$string['loadingcohortdetails'] = 'Loading cohort details...';
-$string['cohortnotfound'] = 'Cohort not found';
-$string['failedtoloadcohortdetails'] = 'Failed to load cohort details. Please try again.';
-$string['backtolist'] = 'Back to List';
-$string['edit'] = 'Edit';
-$string['delete'] = 'Delete';
-$string['retry'] = 'Retry';
-$string['details'] = 'Details';
-$string['members'] = 'Members';
-$string['memberscount'] = 'members';
-$string['basicinformation'] = 'Basic Information';
-$string['visibility'] = 'Visibility';
-$string['nodescriptionprovided'] = 'No description provided.';
-$string['customfields'] = 'Custom Fields';
-$string['cohortmembers'] = 'Cohort Members';
-$string['nomembersfound'] = 'No members found in this cohort.';
-$string['backtocohortlist'] = 'Back to Cohort List';
-
-// CohortEdit.vue strings
 $string['loadingcohortdata'] = 'Loading cohort data...';
 $string['failedtoloadcohortdata'] = 'Failed to load cohort data. Please try again.';
 $string['editcohort'] = 'Edit Cohort';
