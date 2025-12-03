f50ad5d4b2ccb2b5e16857f61b05efd181968339
Davison Almeida <davison.almeida@revvo.com.br>
2025-12-03 20:33:21 -0300

    Traduz strings para português

---
+++ lang/pt_br/local_cohortmanager.php
@@ -0,0 +1,167 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Portuguese language pack for Cohort Manager
+ *
+ * @package    local_cohortmanager
+ * @category   string
+ * @copyright  2025 Davison Almeida <ramosdealmeidasistemas@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Gerenciador de Turmas';
+
+// App component strings
+$string['cohortmanager'] = 'Gerenciador de Turmas';
+$string['newcohort'] = 'Nova Turma';
+$string['searchcohorts'] = 'Buscar turmas...';
+$string['loadingcohorts'] = 'Carregando turmas...';
+$string['name'] = 'Nome';
+$string['idnumber'] = 'Número de ID';
+$string['description'] = 'Descrição';
+$string['visible'] = 'Visível';
+$string['hidden'] = 'Oculto';
+$string['actions'] = 'Ações';
+$string['edit'] = 'Editar';
+$string['delete'] = 'Excluir';
+$string['id'] = 'ID';
+$string['page'] = 'Página';
+$string['of'] = 'de';
+$string['nocoortsfound'] = 'Nenhuma turma encontrada';
+$string['nocoortsfounddesc'] = 'Nenhuma turma encontrada correspondendo aos seus critérios de busca.';
+$string['createyourfirstcohort'] = 'Crie sua primeira turma para começar.';
+$string['createnewcohort'] = 'Criar Nova Turma';
+$string['cancel'] = 'Cancelar';
+$string['save'] = 'Salvar';
+$string['nodecription'] = 'Sem descrição';
+$string['nodescription'] = 'Sem descrição';
+$string['failedtoloadcohorts'] = 'Falha ao carregar turmas. Tente novamente.';
+$string['failedtodeletecohort'] = 'Falha ao excluir turma. Tente novamente.';
+$string['failedtosavecohort'] = 'Falha ao salvar turma. Tente novamente.';
+$string['deleteconfirmation'] = 'Tem certeza de que deseja excluir "%s"?';
+$string['deletethiscohort'] = 'Excluir esta turma';
+$string['cohortdeletedsuccessfully'] = 'Turma excluída com sucesso';
+
+// CohortCreate component strings
+$string['basicinformation'] = 'Informações Básicas';
+$string['cohortnamedescription'] = 'O nome de exibição da turma';
+$string['makecohortvisible'] = 'Tornar a turma visível para os usuários';
+$string['theme'] = 'Tema';
+$string['settings'] = 'Configurações';
+$string['category'] = 'Categoria';
+$string['systemcategory'] = 'Categoria do Sistema';
+$string['coursecategorybyidnumber'] = 'Categoria de Curso por Número de ID';
+$string['coursecategorybyid'] = 'Categoria de Curso por ID';
+$string['entercoursecategoryidnumber'] = 'Digite o número de ID da categoria do curso';
+$string['entercoursecategoryid'] = 'Digite o ID da categoria do curso';
+$string['reset'] = 'Redefinir';
+$string['creating'] = 'Criando...';
+$string['createcohort'] = 'Criar Turma';
+$string['failedtocreatecohort'] = 'Falha ao criar turma. Tente novamente.';
+$string['resetform'] = 'Redefinir Formulário';
+$string['pleaseentercohortname'] = 'Por favor, digite um nome para a turma';
+$string['pleaseenteridnumber'] = 'Por favor, digite um número de ID';
+$string['cohortcreatedsuccessfully'] = 'Turma criada com sucesso';
+$string['cohortname'] = 'Nome da Turma';
+$string['entercohortname'] = 'Digite o nome da turma';
+$string['enteridnumber'] = 'Digite o número de ID';
+$string['idnumberdescription'] = 'O identificador único para a turma';
+$string['entercohortdescription'] = 'Digite a descrição da turma';
+$string['cohortdescription'] = 'Uma breve descrição da turma';
+
+// CohortDetail component strings
+$string['loadingcohortdetails'] = 'Carregando detalhes da turma...';
+$string['cohortnotfound'] = 'Turma Não Encontrada';
+$string['failedtoloadcohortdetails'] = 'Falha ao carregar detalhes da turma. Tente novamente.';
+$string['backtolist'] = 'Voltar à Lista';
+$string['retry'] = 'Tentar Novamente';
+$string['cohortnotfounddetails'] = 'A turma que você está procurando pode ter sido excluída ou não existe.';
+$string['details'] = 'Detalhes';
+$string['members'] = 'Membros';
+$string['memberscount'] = 'membros';
+$string['enrolinstances'] = 'Instâncias de Matrícula';
+$string['visibility'] = 'Visibilidade';
+$string['nodescriptionprovided'] = 'Nenhuma descrição fornecida.';
+$string['customfields'] = 'Campos Personalizados';
+$string['cohortmembers'] = 'Membros da Turma';
+$string['nomembersfound'] = 'Nenhum membro encontrado nesta turma.';
+$string['backtocohortlist'] = 'Voltar à Lista de Turmas';
+$string['username'] = 'Nome de Usuário';
+$string['email'] = 'E-mail';
+
+// CohortEdit component strings
+$string['loadingcohortdata'] = 'Carregando dados da turma...';
+$string['failedtoloadcohortdata'] = 'Falha ao carregar dados da turma. Tente novamente.';
+$string['editcohort'] = 'Editar Turma';
+$string['failedtoupdatecohort'] = 'Falha ao atualizar turma. Tente novamente.';
+$string['saving'] = 'Salvando...';
+$string['savechanges'] = 'Salvar Alterações';
+$string['cohortupdatedsuccessfully'] = 'Turma atualizada com sucesso';
+
+// ThemeSelect component strings
+$string['themedescription'] = 'Selecione um tema para esta turma';
+
+// ContextSelect component strings
+$string['context'] = 'Contexto';
+$string['selectcontext'] = 'Selecionar contexto';
+$string['searchcontext'] = 'Buscar contextos...';
+$string['nocontextsfound'] = 'Nenhum contexto encontrado';
+$string['entercoursecontextid'] = 'Digite o ID do contexto do curso';
+$string['systemcontext'] = 'Contexto do Sistema';
+$string['loading'] = 'Carregando...';
+$string['search'] = 'Buscar';
+
+// CohortDelete component strings
+$string['deletecohort'] = 'Excluir turma';
+
+// CohortCustomFieldsManagement component strings
+$string['customfieldsmanagement'] = 'Gerenciador de campos personalizados da turma';
+$string['back'] = 'Voltar';
+$string['backtohome'] = 'Voltar ao início';
+
+// Custom field form strings
+$string['customfieldssaved'] = 'Campos personalizados salvos com sucesso.';
+$string['errorprocessingcustomfields'] = 'Erro ao processar campos personalizados. Tente novamente.';
+$string['customfieldnotvalid'] = 'Campos personalizados inválidos.';
+
+// CohortMembersAddModal component strings
+$string['addmembers'] = 'Adicionar Membros';
+$string['add'] = 'Adicionar';
+$string['membersadded'] = 'Membros adicionados com sucesso';
+
+// Delete membership confirmation strings
+$string['delete_membership_confirmation'] = 'Tem certeza de que deseja excluir os {$a} membros selecionados desta turma?';
+$string['delete_membership_warning'] = 'Aviso: Esta ação é permanente e não pode ser desfeita. Os membros selecionados serão permanentemente removidos desta turma.';
+$string['selected_members_list'] = 'Membros selecionados: {$a}';
+
+// CohortEnrolInstancesPartial component strings
+$string['cohortenrolinstances'] = 'Instâncias de Matrícula da Turma';
+$string['instancescount'] = 'instâncias';
+$string['loadingenrolinstances'] = 'Carregando instâncias de matrícula...';
+$string['noenrolinstancesfound'] = 'Nenhuma Instância de Matrícula Encontrada';
+$string['noenrolinstancesfounddescription'] = 'Nenhuma instância de matrícula encontrada para esta turma.';
+$string['enrolinstanceslist'] = 'Lista de Instâncias de Matrícula';
+$string['refresh'] = 'Atualizar';
+$string['course'] = 'Curso';
+$string['enrolmethod'] = 'Método de Matrícula';
+$string['status'] = 'Status';
+$string['active'] = 'Ativo';
+$string['inactive'] = 'Inativo';
+$string['cohortenrol'] = 'Turma';
+$string['view'] = 'Visualizar';
