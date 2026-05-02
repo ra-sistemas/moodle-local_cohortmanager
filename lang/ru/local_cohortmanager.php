c08151cecbb13f357bdb8ca485d808667ef03e73
Davison Almeida <davison.almeida@revvo.com.br>
2026-05-02 16:32:43 -0300

    #10 Tradução russo

---
+++ lang/ru/local_cohortmanager.php
@@ -0,0 +1,285 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Russian language pack for Cohort Manager
+ *
+ * @package    local_cohortmanager
+ * @category   string
+ * @copyright  2025 Davison Almeida <ramosdealmeidasistemas@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Менеджер когорт';
+
+// App component strings
+$string['cohortmanager'] = 'Менеджер когорт';
+$string['newcohort'] = 'Новая';
+$string['addcohort'] = 'Добавить когорту';
+$string['searchcohorts'] = 'Поиск...';
+$string['loadingcohorts'] = 'Загрузка...';
+$string['name'] = 'Название';
+$string['idnumber'] = 'ID номер';
+$string['description'] = 'Описание';
+$string['visible'] = 'Видимо';
+$string['hidden'] = 'Скрыто';
+$string['actions'] = 'Действия';
+$string['edit'] = 'Редактировать';
+$string['delete'] = 'Удалить';
+$string['id'] = 'ID';
+$string['page'] = 'Страница';
+$string['of'] = 'из';
+$string['nocoortsfound'] = 'Ничего не найдено';
+$string['nocoortsfounddesc'] = 'Результатов не найдено.';
+$string['createyourfirstcohort'] = 'Создайте первую, чтобы начать.';
+$string['createnewcohort'] = 'Добавить новую';
+$string['cancel'] = 'Отмена';
+$string['save'] = 'Сохранить';
+$string['nodecription'] = 'Без описания';
+$string['nodescription'] = 'Без описания';
+$string['failedtoloadcohorts'] = 'Не удалось загрузить. Попробуйте снова.';
+$string['failedtodeletecohort'] = 'Не удалось удалить. Попробуйте снова.';
+$string['failedtosavecohort'] = 'Не удалось сохранить. Попробуйте снова.';
+$string['deleteconfirmation'] = 'Вы уверены, что хотите удалить "%s"?';
+$string['deletethiscohort'] = 'Удалить';
+$string['cohortdeletedsuccessfully'] = 'Успешно удалено';
+$string['instanceexists'] = 'Экземпляр зачисления с этой когортой и ролью уже существует в курсе. Попробуйте другую роль.';
+
+// CohortCreate component strings
+$string['basicinformation'] = 'Основная информация';
+$string['cohortnamedescription'] = 'Отображаемое имя';
+$string['makecohortvisible'] = 'Сделать видимым для пользователей';
+$string['theme'] = 'Тема';
+$string['settings'] = 'Настройки';
+$string['category'] = 'Категория';
+$string['systemcategory'] = 'Система';
+$string['coursecategorybyidnumber'] = 'По ID номеру';
+$string['coursecategorybyid'] = 'По ID';
+$string['entercoursecategoryidnumber'] = 'Введите ID номер категории';
+$string['entercoursecategoryid'] = 'Введите ID категории';
+$string['reset'] = 'Сбросить';
+$string['creating'] = 'Добавление...';
+$string['createcohort'] = 'Добавить';
+$string['failedtocreatecohort'] = 'Не удалось добавить. Попробуйте снова.';
+$string['resetform'] = 'Сбросить форму';
+$string['pleaseentercohortname'] = 'Пожалуйста, введите название';
+$string['pleaseenteridnumber'] = 'Пожалуйста, введите ID номер';
+$string['cohortcreatedsuccessfully'] = 'Успешно добавлено';
+$string['cohortname'] = 'Название';
+$string['entercohortname'] = 'Введите название';
+$string['enteridnumber'] = 'Введите ID номер';
+$string['idnumberdescription'] = 'Уникальный идентификатор';
+$string['entercohortdescription'] = 'Введите описание';
+$string['cohortdescription'] = 'Краткое описание';
+
+// CohortDetail component strings
+$string['loadingcohortdetails'] = 'Загрузка деталей...';
+$string['cohortnotfound'] = 'Не найдено';
+$string['failedtoloadcohortdetails'] = 'Не удалось загрузить детали. Попробуйте снова.';
+$string['backtolist'] = 'Назад к списку';
+$string['retry'] = 'Повторить';
+$string['cohortnotfounddetails'] = 'Возможно, элемент был удален или не существует.';
+$string['details'] = 'Детали';
+$string['cohortportrait'] = 'Обзор когорты';
+$string['members'] = 'Участники';
+$string['memberscount'] = 'участников';
+$string['enrolinstances'] = 'Экземпляры зачисления';
+$string['totalenrolments'] = 'Всего зачислено';
+$string['roleassignments'] = 'Назначения ролей';
+$string['visibility'] = 'Видимость';
+$string['nodescriptionprovided'] = 'Описание не предоставлено.';
+$string['customfields'] = 'Пользовательские поля';
+$string['cohortmembers'] = 'Участники';
+$string['nomembersfound'] = 'Участники не найдены';
+$string['nomembersfounddescription'] = 'В этой когорте нет участников. Добавьте участников, чтобы начать.';
+$string['backtocohortlist'] = 'Назад к списку';
+$string['username'] = 'Имя пользователя';
+$string['email'] = 'Эл. почта';
+
+// CohortEdit component strings
+$string['loadingcohortdata'] = 'Загрузка данных...';
+$string['failedtoloadcohortdata'] = 'Не удалось загрузить данные. Попробуйте снова.';
+$string['editcohort'] = 'Редактировать';
+$string['failedtoupdatecohort'] = 'Не удалось обновить. Попробуйте снова.';
+$string['saving'] = 'Сохранение...';
+$string['savechanges'] = 'Сохранить изменения';
+$string['cohortupdatedsuccessfully'] = 'Успешно обновлено';
+
+// ThemeSelect component strings
+$string['themedescription'] = 'Выберите тему';
+
+// ContextSelect component strings
+$string['context'] = 'Контекст';
+$string['selectcontext'] = 'Выберите контекст';
+$string['searchcontext'] = 'Поиск контекстов...';
+$string['nocontextsfound'] = 'Контексты не найдены';
+$string['entercoursecontextid'] = 'Введите ID контекста курса';
+$string['systemcontext'] = 'Система';
+$string['loading'] = 'Загрузка...';
+$string['search'] = 'Поиск';
+
+// CohortDelete component strings
+$string['deletecohort'] = 'Удалить';
+
+// CohortCustomFieldsManagement component strings
+$string['customfieldsmanagement'] = 'Управление пользовательскими полями';
+$string['back'] = 'Назад';
+$string['backtohome'] = 'На главную';
+
+// Custom field form strings
+$string['customfieldssaved'] = 'Пользовательские поля сохранены.';
+$string['errorprocessingcustomfields'] = 'Ошибка обработки пользовательских полей. Попробуйте снова.';
+$string['customfieldnotvalid'] = 'Недействительные пользовательские поля.';
+
+// CohortMembersAddModal component strings
+$string['addmembers'] = 'Добавить участников';
+$string['add'] = 'Добавить';
+$string['membersadded'] = 'Участники успешно добавлены';
+
+// Delete membership confirmation strings
+$string['delete_membership_confirmation'] = 'Вы уверены, что хотите удалить выбранных {$a} участников?';
+$string['delete_membership_warning'] = 'Предупреждение: Это действие является постоянным и необратимым.';
+$string['selected_members_list'] = 'Выбрано: {$a}';
+
+// CohortEnrolInstancesPartial component strings
+$string['addenrolinstance'] = 'Добавить экземпляр зачисления';
+$string['cohortenrolinstances'] = 'Экземпляры зачисления';
+$string['instancescount'] = 'экземпляров';
+$string['loadingenrolinstances'] = 'Загрузка экземпляров зачисления...';
+$string['noenrolinstancesfound'] = 'Экземпляры зачисления не найдены';
+$string['noenrolinstancesfounddescription'] = 'Экземпляры зачисления не найдены.';
+$string['enrolinstanceslist'] = 'Экземпляры зачисления';
+$string['refresh'] = 'Обновить';
+$string['course'] = 'Курс';
+$string['enrolmethod'] = 'Метод';
+$string['status'] = 'Статус';
+$string['active'] = 'Активен';
+$string['inactive'] = 'Неактивен';
+$string['cohortenrol'] = 'Когорта';
+$string['view'] = 'Просмотр';
+$string['deleteenrolinstance'] = 'Удалить экземпляр зачисления';
+$string['deleteenrolinstanceconfirmation'] = 'Вы уверены, что хотите удалить экземпляр зачисления для "%s"?';
+$string['deleteenrolinstancewarning'] = 'Предупреждение: Это удалит {$a} зачислений пользователей из этого курса.';
+$string['enrolinstancedeletedsuccessfully'] = 'Экземпляр зачисления успешно удален';
+$string['searchenrolinstances'] = 'Поиск экземпляров зачисления...';
+$string['filterbystatus'] = 'Фильтр по статусу';
+$string['all'] = 'Все';
+$string['togglestatus'] = 'Переключить статус';
+$string['enrolinstancestatusupdated'] = 'Статус экземпляра зачисления обновлен';
+$string['errortogglingstatus'] = 'Ошибка при обновлении статуса экземпляра зачисления';
+$string['enrolinstanceupdated'] = 'Экземпляр зачисления успешно обновлен';
+
+// Roles management component strings
+$string['rolesmanagement'] = 'Управление ролями';
+$string['usercontextroles'] = 'Роли в контексте пользователя';
+$string['searchroles'] = 'Поиск ролей...';
+$string['loadingroles'] = 'Загрузка ролей...';
+$string['rolename'] = 'Название роли';
+$string['roleshortname'] = 'Краткое название';
+$string['roleid'] = 'ID';
+$string['rolesortorder'] = 'Порядок';
+$string['rolearchetype'] = 'Архетип';
+$string['roleactions'] = 'Действия';
+$string['editrole'] = 'Редактировать';
+$string['deleterole'] = 'Удалить';
+$string['createnewrole'] = 'Создать новую роль';
+$string['addnewrole'] = 'Добавить новую роль';
+$string['rolecreatedsuccessfully'] = 'Роль успешно создана';
+$string['roleupdatedsuccessfully'] = 'Роль успешно обновлена';
+$string['roledeletedsuccessfully'] = 'Роль успешно удалена';
+$string['rolenotfound'] = 'Роль не найдена';
+$string['roleexists'] = 'Роль с таким кратким названием уже существует';
+$string['noassignableroles'] = 'Нет доступных ролей для назначения в контексте пользователя.';
+$string['roledescriptionplaceholder'] = 'Введите описание (необязательно)';
+$string['rolearchetypeplaceholder'] = 'Выберите архетип (необязательно)';
+$string['rolecreate'] = 'Создать роль';
+$string['roleupdate'] = 'Обновить роль';
+$string['roledeleteconfirmation'] = 'Вы уверены, что хотите удалить "%s"? Это действие нельзя отменить.';
+$string['deletethisrole'] = 'Удалить эту роль';
+$string['cannotdeletethisrole'] = 'Эта роль не может быть удалена, так как является системной.';
+$string['cannotdeleterolewithid'] = 'Невозможно удалить роль с ID {$a}.';
+$string['rolesearchplaceholder'] = 'Поиск по названию, краткому или пользовательскому имени...';
+$string['rolescount'] = 'ролей';
+$string['norolesfound'] = 'Роли не найдены';
+$string['norolesfounddesc'] = 'Роли не найдены.';
+$string['createyourfirstrole'] = 'Создайте свою первую роль, чтобы начать.';
+
+// DynamicCohortMembersTable component strings
+$string['searchmembers'] = 'Поиск участников...';
+$string['first'] = 'Первая';
+$string['last'] = 'Последняя';
+$string['columns'] = 'Столбцы';
+$string['city'] = 'Город';
+$string['country'] = 'Страна';
+$string['showing'] = 'Показано';
+$string['to'] = 'по';
+$string['previous'] = 'Предыдущая';
+$string['next'] = 'Следующая';
+$string['failedtofetchmembers'] = 'Не удалось получить участников';
+$string['anerroroccurred'] = 'Произошла ошибка';
+$string['unknown'] = 'Неизвестно';
+$string['errordeletingmembers'] = 'Ошибка при удалении участников';
+
+// CohortEnrolInstancesSearchModal component strings
+$string['searchcourses'] = 'Поиск курсов';
+$string['searchcoursesplaceholder'] = 'Поиск курсов...';
+$string['searching'] = 'Поиск...';
+$string['nocoursesfound'] = 'Курсы не найдены';
+$string['selectedcourses'] = 'Выбранные курсы';
+$string['coursealreadyselected'] = 'Курс уже выбран';
+$string['noselectedcourses'] = 'Курсы не выбраны';
+$string['role'] = 'Роль';
+$string['group'] = 'Группа';
+$string['remove'] = 'Удалить';
+$string['enroled'] = 'Зачислены';
+$string['groupmembers'] = 'Участники группы';
+$string['enrolinstanceadded'] = 'Экземпляр зачисления успешно добавлен';
+$string['errorcreatingenrolinstances'] = 'Ошибка при создании экземпляров зачисления. Попробуйте снова.';
+
+// Cohort role assignments (tool_cohortroles integration) strings
+$string['cohortroles'] = 'Роли когорты';
+$string['cohortroleassignments'] = 'Назначения';
+$string['cohortroleassignmentscount'] = 'назначений';
+$string['loadingcohortroles'] = 'Загрузка назначений ролей...';
+$string['noroughassignmentsfound'] = 'Назначения не найдены';
+$string['noroughassignmentsfounddescription'] = 'Назначения не найдены. Назначьте пользователям роли для установления иерархии.';
+$string['createyourfirstassignment'] = 'Создайте свое первое назначение, чтобы начать.';
+$string['addroleassignment'] = 'Добавить назначение';
+$string['assigneduser'] = 'Пользователь';
+$string['assignedrole'] = 'Роль';
+$string['assignedcohort'] = 'Когорта';
+$string['assignedtime'] = 'Назначено';
+$string['deleteassignment'] = 'Удалить назначение';
+$string['deleteassignmentconfirmation'] = 'Вы уверены, что хотите удалить это назначение? Оно будет удалено при следующей синхронизации.';
+$string['cohortroleassignmentcreated'] = 'Назначение создано. Вступит в силу после следующей синхронизации.';
+$string['cohortroleassignmentdeleted'] = 'Назначение успешно удалено.';
+$string['cohortroleassignmentexists'] = 'Это назначение уже существует.';
+$string['backgroundsyncnote'] = 'Назначения вступают в силу после запуска плановой синхронизации.';
+$string['synclastsync'] = 'Последняя';
+$string['syncnextsync'] = 'Следующая';
+$string['syncnever'] = 'Никогда';
+$string['selectuser'] = 'Выберите пользователя';
+$string['searchusers'] = 'Поиск пользователей...';
+$string['selectrole'] = 'Выберите роль';
+$string['nousersfound'] = 'Пользователи не найдены';
+$string['assignroletouser'] = 'Назначить роль';
+$string['userrolecohortinfo'] = 'Назначает роль в контексте пользователя для всех участников.';
+$string['assignedon'] = 'Назначено';
+$string['deletethisassignment'] = 'Удалить это назначение';
+$string['assigneduserscount'] = 'Назначения';
+$string['formvalidationerror'] = 'Пожалуйста, исправьте ошибки в форме.';
