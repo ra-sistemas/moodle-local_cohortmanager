5516efd6fb92b76dffb8f40680b782b7c62559df
Davison Almeida <davison.almeida@revvo.com.br>
2026-05-02 16:32:26 -0300

    #10 Tradução neozelandez

---
+++ lang/nl/local_cohortmanager.php
@@ -0,0 +1,285 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Dutch language pack for Cohort Manager
+ *
+ * @package    local_cohortmanager
+ * @category   string
+ * @copyright  2025 Davison Almeida <ramosdealmeidasistemas@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Cohortbeheerder';
+
+// App component strings
+$string['cohortmanager'] = 'Cohortbeheerder';
+$string['newcohort'] = 'Nieuw';
+$string['addcohort'] = 'Cohort toevoegen';
+$string['searchcohorts'] = 'Zoeken...';
+$string['loadingcohorts'] = 'Laden...';
+$string['name'] = 'Naam';
+$string['idnumber'] = 'ID-nummer';
+$string['description'] = 'Beschrijving';
+$string['visible'] = 'Zichtbaar';
+$string['hidden'] = 'Verborgen';
+$string['actions'] = 'Acties';
+$string['edit'] = 'Bewerken';
+$string['delete'] = 'Verwijderen';
+$string['id'] = 'ID';
+$string['page'] = 'Pagina';
+$string['of'] = 'van';
+$string['nocoortsfound'] = 'Geen gevonden';
+$string['nocoortsfounddesc'] = 'Geen resultaten gevonden.';
+$string['createyourfirstcohort'] = 'Maak uw eerste om te beginnen.';
+$string['createnewcohort'] = 'Nieuw Toevoegen';
+$string['cancel'] = 'Annuleren';
+$string['save'] = 'Opslaan';
+$string['nodecription'] = 'Geen beschrijving';
+$string['nodescription'] = 'Geen beschrijving';
+$string['failedtoloadcohorts'] = 'Laden mislukt. Probeer opnieuw.';
+$string['failedtodeletecohort'] = 'Verwijderen mislukt. Probeer opnieuw.';
+$string['failedtosavecohort'] = 'Opslaan mislukt. Probeer opnieuw.';
+$string['deleteconfirmation'] = 'Weet u zeker dat u "%s" wilt verwijderen?';
+$string['deletethiscohort'] = 'Verwijderen';
+$string['cohortdeletedsuccessfully'] = 'Succesvol verwijderd';
+$string['instanceexists'] = 'Er bestaat al een aanmeldingsexemplaar met dit cohort en deze rol in de cursus. Probeer een andere rol.';
+
+// CohortCreate component strings
+$string['basicinformation'] = 'Basisinformatie';
+$string['cohortnamedescription'] = 'De weergavenaam';
+$string['makecohortvisible'] = 'Zichtbaar maken voor gebruikers';
+$string['theme'] = 'Thema';
+$string['settings'] = 'Instellingen';
+$string['category'] = 'Categorie';
+$string['systemcategory'] = 'Systeem';
+$string['coursecategorybyidnumber'] = 'Op ID-nummer';
+$string['coursecategorybyid'] = 'Op ID';
+$string['entercoursecategoryidnumber'] = 'Voer het categorie-ID-nummer in';
+$string['entercoursecategoryid'] = 'Voer de categorie-ID in';
+$string['reset'] = 'Resetten';
+$string['creating'] = 'Toevoegen...';
+$string['createcohort'] = 'Toevoegen';
+$string['failedtocreatecohort'] = 'Toevoegen mislukt. Probeer opnieuw.';
+$string['resetform'] = 'Formulier Resetten';
+$string['pleaseentercohortname'] = 'Voer een naam in';
+$string['pleaseenteridnumber'] = 'Voer een ID-nummer in';
+$string['cohortcreatedsuccessfully'] = 'Succesvol toegevoegd';
+$string['cohortname'] = 'Naam';
+$string['entercohortname'] = 'Voer naam in';
+$string['enteridnumber'] = 'Voer ID-nummer in';
+$string['idnumberdescription'] = 'De unieke identificatie';
+$string['entercohortdescription'] = 'Voer beschrijving in';
+$string['cohortdescription'] = 'Een korte beschrijving';
+
+// CohortDetail component strings
+$string['loadingcohortdetails'] = 'Details laden...';
+$string['cohortnotfound'] = 'Niet Gevonden';
+$string['failedtoloadcohortdetails'] = 'Details laden mislukt. Probeer opnieuw.';
+$string['backtolist'] = 'Terug naar Lijst';
+$string['retry'] = 'Opnieuw';
+$string['cohortnotfounddetails'] = 'Het item dat u zoekt is mogelijk verwijderd of bestaat niet.';
+$string['details'] = 'Details';
+$string['cohortportrait'] = 'Cohortoverzicht';
+$string['members'] = 'Leden';
+$string['memberscount'] = 'leden';
+$string['enrolinstances'] = 'Aanmeldingsexemplaren';
+$string['totalenrolments'] = 'Totaal Aanmeldingen';
+$string['roleassignments'] = 'Roltoewijzingen';
+$string['visibility'] = 'Zichtbaarheid';
+$string['nodescriptionprovided'] = 'Geen beschrijving opgegeven.';
+$string['customfields'] = 'Aangepaste Velden';
+$string['cohortmembers'] = 'Leden';
+$string['nomembersfound'] = 'Geen Leden Gevonden';
+$string['nomembersfounddescription'] = 'Geen leden gevonden in dit cohort. Voeg leden toe om te beginnen.';
+$string['backtocohortlist'] = 'Terug naar Lijst';
+$string['username'] = 'Gebruikersnaam';
+$string['email'] = 'E-mail';
+
+// CohortEdit component strings
+$string['loadingcohortdata'] = 'Gegevens laden...';
+$string['failedtoloadcohortdata'] = 'Gegevens laden mislukt. Probeer opnieuw.';
+$string['editcohort'] = 'Bewerken';
+$string['failedtoupdatecohort'] = 'Bijwerken mislukt. Probeer opnieuw.';
+$string['saving'] = 'Opslaan...';
+$string['savechanges'] = 'Wijzigingen Opslaan';
+$string['cohortupdatedsuccessfully'] = 'Succesvol bijgewerkt';
+
+// ThemeSelect component strings
+$string['themedescription'] = 'Selecteer een thema';
+
+// ContextSelect component strings
+$string['context'] = 'Context';
+$string['selectcontext'] = 'Selecteer context';
+$string['searchcontext'] = 'Contexten zoeken...';
+$string['nocontextsfound'] = 'Geen contexten gevonden';
+$string['entercoursecontextid'] = 'Voer de cursuscontext-ID in';
+$string['systemcontext'] = 'Systeem';
+$string['loading'] = 'Laden...';
+$string['search'] = 'Zoeken';
+
+// CohortDelete component strings
+$string['deletecohort'] = 'Verwijderen';
+
+// CohortCustomFieldsManagement component strings
+$string['customfieldsmanagement'] = 'Beheerder Aangepaste Velden';
+$string['back'] = 'Terug';
+$string['backtohome'] = 'Terug naar start';
+
+// Custom field form strings
+$string['customfieldssaved'] = 'Aangepaste velden succesvol opgeslagen.';
+$string['errorprocessingcustomfields'] = 'Fout bij verwerken van aangepaste velden. Probeer opnieuw.';
+$string['customfieldnotvalid'] = 'Ongeldige aangepaste velden.';
+
+// CohortMembersAddModal component strings
+$string['addmembers'] = 'Leden Toevoegen';
+$string['add'] = 'Toevoegen';
+$string['membersadded'] = 'Leden succesvol toegevoegd';
+
+// Delete membership confirmation strings
+$string['delete_membership_confirmation'] = 'Weet u zeker dat u de geselecteerde {$a} leden wilt verwijderen?';
+$string['delete_membership_warning'] = 'Waarschuwing: Deze actie is permanent en onomkeerbaar.';
+$string['selected_members_list'] = 'Geselecteerd: {$a}';
+
+// CohortEnrolInstancesPartial component strings
+$string['addenrolinstance'] = 'Aanmeldingsexemplaar Toevoegen';
+$string['cohortenrolinstances'] = 'Aanmeldingsexemplaren';
+$string['instancescount'] = 'exemplaren';
+$string['loadingenrolinstances'] = 'Aanmeldingsexemplaren laden...';
+$string['noenrolinstancesfound'] = 'Geen Aanmeldingsexemplaren Gevonden';
+$string['noenrolinstancesfounddescription'] = 'Geen aanmeldingsexemplaren gevonden.';
+$string['enrolinstanceslist'] = 'Aanmeldingsexemplaren';
+$string['refresh'] = 'Verversen';
+$string['course'] = 'Cursus';
+$string['enrolmethod'] = 'Methode';
+$string['status'] = 'Status';
+$string['active'] = 'Actief';
+$string['inactive'] = 'Inactief';
+$string['cohortenrol'] = 'Cohort';
+$string['view'] = 'Bekijken';
+$string['deleteenrolinstance'] = 'Aanmeldingsexemplaar verwijderen';
+$string['deleteenrolinstanceconfirmation'] = 'Weet u zeker dat u het aanmeldingsexemplaar voor "%s" wilt verwijderen?';
+$string['deleteenrolinstancewarning'] = 'Waarschuwing: Dit verwijdert {$a} gebruikersaanmeldingen uit deze cursus.';
+$string['enrolinstancedeletedsuccessfully'] = 'Aanmeldingsexemplaar succesvol verwijderd';
+$string['searchenrolinstances'] = 'Zoek aanmeldingsexemplaren...';
+$string['filterbystatus'] = 'Filteren op status';
+$string['all'] = 'Alle';
+$string['togglestatus'] = 'Status wijzigen';
+$string['enrolinstancestatusupdated'] = 'Status aanmeldingsexemplaar succesvol bijgewerkt';
+$string['errortogglingstatus'] = 'Fout bij bijwerken status aanmeldingsexemplaar';
+$string['enrolinstanceupdated'] = 'Aanmeldingsexemplaar succesvol bijgewerkt';
+
+// Roles management component strings
+$string['rolesmanagement'] = 'Rollenbeheer';
+$string['usercontextroles'] = 'Rollen in Gebruikerscontext';
+$string['searchroles'] = 'Rollen zoeken...';
+$string['loadingroles'] = 'Rollen laden...';
+$string['rolename'] = 'Rolnaam';
+$string['roleshortname'] = 'Korte Naam';
+$string['roleid'] = 'ID';
+$string['rolesortorder'] = 'Volgorde';
+$string['rolearchetype'] = 'Archetype';
+$string['roleactions'] = 'Acties';
+$string['editrole'] = 'Bewerken';
+$string['deleterole'] = 'Verwijderen';
+$string['createnewrole'] = 'Nieuwe Rol Aanmaken';
+$string['addnewrole'] = 'Nieuwe rol toevoegen';
+$string['rolecreatedsuccessfully'] = 'Rol succesvol aangemaakt';
+$string['roleupdatedsuccessfully'] = 'Rol succesvol bijgewerkt';
+$string['roledeletedsuccessfully'] = 'Rol succesvol verwijderd';
+$string['rolenotfound'] = 'Rol niet gevonden';
+$string['roleexists'] = 'Een rol met deze korte naam bestaat al';
+$string['noassignableroles'] = 'Geen toewijsbare rollen beschikbaar in gebruikerscontext.';
+$string['roledescriptionplaceholder'] = 'Voer beschrijving in (optioneel)';
+$string['rolearchetypeplaceholder'] = 'Selecteer archetype (optioneel)';
+$string['rolecreate'] = 'Rol Aanmaken';
+$string['roleupdate'] = 'Rol Bijwerken';
+$string['roledeleteconfirmation'] = 'Weet u zeker dat u "%s" wilt verwijderen? Deze actie kan niet ongedaan worden gemaakt.';
+$string['deletethisrole'] = 'Verwijder deze rol';
+$string['cannotdeletethisrole'] = 'Deze rol kan niet worden verwijderd omdat het een systeemrol is.';
+$string['cannotdeleterolewithid'] = 'Kan rol met ID {$a} niet verwijderen.';
+$string['rolesearchplaceholder'] = 'Zoeken op naam, korte naam of aangepaste naam...';
+$string['rolescount'] = 'rollen';
+$string['norolesfound'] = 'Geen rollen gevonden';
+$string['norolesfounddesc'] = 'Geen rollen gevonden.';
+$string['createyourfirstrole'] = 'Maak uw eerste rol om te beginnen.';
+
+// DynamicCohortMembersTable component strings
+$string['searchmembers'] = 'Leden zoeken...';
+$string['first'] = 'Eerste';
+$string['last'] = 'Laatste';
+$string['columns'] = 'Kolommen';
+$string['city'] = 'Stad';
+$string['country'] = 'Land';
+$string['showing'] = 'Weergeven';
+$string['to'] = 'tot';
+$string['previous'] = 'Vorige';
+$string['next'] = 'Volgende';
+$string['failedtofetchmembers'] = 'Ophalen van leden mislukt';
+$string['anerroroccurred'] = 'Er is een fout opgetreden';
+$string['unknown'] = 'Onbekend';
+$string['errordeletingmembers'] = 'Fout bij verwijderen van leden';
+
+// CohortEnrolInstancesSearchModal component strings
+$string['searchcourses'] = 'Cursussen zoeken';
+$string['searchcoursesplaceholder'] = 'Cursussen zoeken...';
+$string['searching'] = 'Zoeken...';
+$string['nocoursesfound'] = 'Geen cursussen gevonden';
+$string['selectedcourses'] = 'Geselecteerde cursussen';
+$string['coursealreadyselected'] = 'Cursus al geselecteerd';
+$string['noselectedcourses'] = 'Geen cursussen geselecteerd';
+$string['role'] = 'Rol';
+$string['group'] = 'Groep';
+$string['remove'] = 'Verwijderen';
+$string['enroled'] = 'Aangemeld';
+$string['groupmembers'] = 'Groepsleden';
+$string['enrolinstanceadded'] = 'Aanmeldingsexemplaar succesvol toegevoegd';
+$string['errorcreatingenrolinstances'] = 'Fout bij aanmaken van aanmeldingsexemplaren. Probeer opnieuw.';
+
+// Cohort role assignments (tool_cohortroles integration) strings
+$string['cohortroles'] = 'Cohortrollen';
+$string['cohortroleassignments'] = 'Toewijzingen';
+$string['cohortroleassignmentscount'] = 'toewijzingen';
+$string['loadingcohortroles'] = 'Roltoewijzingen laden...';
+$string['noroughassignmentsfound'] = 'Geen Roltoewijzingen Gevonden';
+$string['noroughassignmentsfounddescription'] = 'Geen toewijzingen gevonden. Wijs gebruikers met rollen toe om hiërarchie vast te stellen.';
+$string['createyourfirstassignment'] = 'Maak uw eerste toewijzing om te beginnen.';
+$string['addroleassignment'] = 'Toewijzing Toevoegen';
+$string['assigneduser'] = 'Gebruiker';
+$string['assignedrole'] = 'Rol';
+$string['assignedcohort'] = 'Cohort';
+$string['assignedtime'] = 'Toegewezen Op';
+$string['deleteassignment'] = 'Toewijzing verwijderen';
+$string['deleteassignmentconfirmation'] = 'Weet u zeker dat u deze toewijzing wilt verwijderen? Deze wordt verwijderd bij de volgende synchronisatie.';
+$string['cohortroleassignmentcreated'] = 'Toewijzing aangemaakt. Wordt van kracht na de volgende synchronisatie.';
+$string['cohortroleassignmentdeleted'] = 'Toewijzing succesvol verwijderd.';
+$string['cohortroleassignmentexists'] = 'Deze toewijzing bestaat al.';
+$string['backgroundsyncnote'] = 'Toewijzingen worden van kracht nadat de geplande synchronisatie is uitgevoerd.';
+$string['synclastsync'] = 'Laatste';
+$string['syncnextsync'] = 'Volgende';
+$string['syncnever'] = 'Nooit';
+$string['selectuser'] = 'Selecteer een gebruiker';
+$string['searchusers'] = 'Gebruikers zoeken...';
+$string['selectrole'] = 'Selecteer een rol';
+$string['nousersfound'] = 'Geen gebruikers gevonden';
+$string['assignroletouser'] = 'Rol Toewijzen';
+$string['userrolecohortinfo'] = 'Wijst een gebruikerscontextrol toe aan alle leden.';
+$string['assignedon'] = 'Toegewezen op';
+$string['deletethisassignment'] = 'Verwijder deze toewijzing';
+$string['assigneduserscount'] = 'Toewijzingen';
+$string['formvalidationerror'] = 'Corrigeer de fouten in het formulier.';
