f5079c79d53aea1582adf53756eace467ca2023f
Davison Almeida <davison.almeida@revvo.com.br>
2026-05-02 16:31:37 -0300

    #10 Tradução alemão

---
+++ lang/de/local_cohortmanager.php
@@ -0,0 +1,285 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * German language pack for Cohort Manager
+ *
+ * @package    local_cohortmanager
+ * @category   string
+ * @copyright  2025 Davison Almeida <ramosdealmeidasistemas@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Kohortenverwaltung';
+
+// App component strings
+$string['cohortmanager'] = 'Kohortenverwaltung';
+$string['newcohort'] = 'Neu';
+$string['addcohort'] = 'Kohorte hinzufügen';
+$string['searchcohorts'] = 'Suchen...';
+$string['loadingcohorts'] = 'Laden...';
+$string['name'] = 'Name';
+$string['idnumber'] = 'ID-Nummer';
+$string['description'] = 'Beschreibung';
+$string['visible'] = 'Sichtbar';
+$string['hidden'] = 'Versteckt';
+$string['actions'] = 'Aktionen';
+$string['edit'] = 'Bearbeiten';
+$string['delete'] = 'Löschen';
+$string['id'] = 'ID';
+$string['page'] = 'Seite';
+$string['of'] = 'von';
+$string['nocoortsfound'] = 'Keine gefunden';
+$string['nocoortsfounddesc'] = 'Keine Ergebnisse gefunden.';
+$string['createyourfirstcohort'] = 'Erstellen Sie Ihre erste, um zu beginnen.';
+$string['createnewcohort'] = 'Neu Hinzufügen';
+$string['cancel'] = 'Abbrechen';
+$string['save'] = 'Speichern';
+$string['nodecription'] = 'Keine Beschreibung';
+$string['nodescription'] = 'Keine Beschreibung';
+$string['failedtoloadcohorts'] = 'Laden fehlgeschlagen. Bitte versuchen Sie es erneut.';
+$string['failedtodeletecohort'] = 'Löschen fehlgeschlagen. Bitte versuchen Sie es erneut.';
+$string['failedtosavecohort'] = 'Speichern fehlgeschlagen. Bitte versuchen Sie es erneut.';
+$string['deleteconfirmation'] = 'Sind Sie sicher, dass Sie "%s" löschen möchten?';
+$string['deletethiscohort'] = 'Löschen';
+$string['cohortdeletedsuccessfully'] = 'Erfolgreich gelöscht';
+$string['instanceexists'] = 'Eine Einschreibeinstanz mit dieser Kohorte und Rolle im Kurs existiert bereits. Versuchen Sie eine andere Rolle.';
+
+// CohortCreate component strings
+$string['basicinformation'] = 'Grundinformationen';
+$string['cohortnamedescription'] = 'Der Anzeigename';
+$string['makecohortvisible'] = 'Für Benutzer sichtbar machen';
+$string['theme'] = 'Design';
+$string['settings'] = 'Einstellungen';
+$string['category'] = 'Kategorie';
+$string['systemcategory'] = 'System';
+$string['coursecategorybyidnumber'] = 'Nach ID-Nummer';
+$string['coursecategorybyid'] = 'Nach ID';
+$string['entercoursecategoryidnumber'] = 'Geben Sie die Kategorie-ID-Nummer ein';
+$string['entercoursecategoryid'] = 'Geben Sie die Kategorie-ID ein';
+$string['reset'] = 'Zurücksetzen';
+$string['creating'] = 'Hinzufügen...';
+$string['createcohort'] = 'Hinzufügen';
+$string['failedtocreatecohort'] = 'Hinzufügen fehlgeschlagen. Bitte versuchen Sie es erneut.';
+$string['resetform'] = 'Formular Zurücksetzen';
+$string['pleaseentercohortname'] = 'Bitte geben Sie einen Namen ein';
+$string['pleaseenteridnumber'] = 'Bitte geben Sie eine ID-Nummer ein';
+$string['cohortcreatedsuccessfully'] = 'Erfolgreich hinzugefügt';
+$string['cohortname'] = 'Name';
+$string['entercohortname'] = 'Namen eingeben';
+$string['enteridnumber'] = 'ID-Nummer eingeben';
+$string['idnumberdescription'] = 'Die eindeutige Kennung';
+$string['entercohortdescription'] = 'Beschreibung eingeben';
+$string['cohortdescription'] = 'Eine kurze Beschreibung';
+
+// CohortDetail component strings
+$string['loadingcohortdetails'] = 'Details werden geladen...';
+$string['cohortnotfound'] = 'Nicht Gefunden';
+$string['failedtoloadcohortdetails'] = 'Details konnten nicht geladen werden. Bitte versuchen Sie es erneut.';
+$string['backtolist'] = 'Zurück zur Liste';
+$string['retry'] = 'Erneut versuchen';
+$string['cohortnotfounddetails'] = 'Das Element wurde möglicherweise gelöscht oder existiert nicht.';
+$string['details'] = 'Details';
+$string['cohortportrait'] = 'Kohortenporträt';
+$string['members'] = 'Mitglieder';
+$string['memberscount'] = 'Mitglieder';
+$string['enrolinstances'] = 'Einschreibeinstanzen';
+$string['totalenrolments'] = 'Einschreibungen Gesamt';
+$string['roleassignments'] = 'Rollenzuweisungen';
+$string['visibility'] = 'Sichtbarkeit';
+$string['nodescriptionprovided'] = 'Keine Beschreibung angegeben.';
+$string['customfields'] = 'Benutzerdefinierte Felder';
+$string['cohortmembers'] = 'Mitglieder';
+$string['nomembersfound'] = 'Keine Mitglieder Gefunden';
+$string['nomembersfounddescription'] = 'Keine Mitglieder in dieser Kohorte gefunden. Fügen Sie Mitglieder hinzu, um zu beginnen.';
+$string['backtocohortlist'] = 'Zurück zur Liste';
+$string['username'] = 'Benutzername';
+$string['email'] = 'E-Mail';
+
+// CohortEdit component strings
+$string['loadingcohortdata'] = 'Daten werden geladen...';
+$string['failedtoloadcohortdata'] = 'Daten konnten nicht geladen werden. Bitte versuchen Sie es erneut.';
+$string['editcohort'] = 'Bearbeiten';
+$string['failedtoupdatecohort'] = 'Aktualisierung fehlgeschlagen. Bitte versuchen Sie es erneut.';
+$string['saving'] = 'Speichern...';
+$string['savechanges'] = 'Änderungen Speichern';
+$string['cohortupdatedsuccessfully'] = 'Erfolgreich aktualisiert';
+
+// ThemeSelect component strings
+$string['themedescription'] = 'Wählen Sie ein Design';
+
+// ContextSelect component strings
+$string['context'] = 'Kontext';
+$string['selectcontext'] = 'Kontext auswählen';
+$string['searchcontext'] = 'Kontexte suchen...';
+$string['nocontextsfound'] = 'Keine Kontexte gefunden';
+$string['entercoursecontextid'] = 'Geben Sie die Kurskontext-ID ein';
+$string['systemcontext'] = 'System';
+$string['loading'] = 'Laden...';
+$string['search'] = 'Suchen';
+
+// CohortDelete component strings
+$string['deletecohort'] = 'Löschen';
+
+// CohortCustomFieldsManagement component strings
+$string['customfieldsmanagement'] = 'Verwaltung benutzerdefinierter Felder';
+$string['back'] = 'Zurück';
+$string['backtohome'] = 'Zurück zur Startseite';
+
+// Custom field form strings
+$string['customfieldssaved'] = 'Benutzerdefinierte Felder erfolgreich gespeichert.';
+$string['errorprocessingcustomfields'] = 'Fehler bei der Verarbeitung benutzerdefinierter Felder. Bitte versuchen Sie es erneut.';
+$string['customfieldnotvalid'] = 'Ungültige benutzerdefinierte Felder.';
+
+// CohortMembersAddModal component strings
+$string['addmembers'] = 'Mitglieder Hinzufügen';
+$string['add'] = 'Hinzufügen';
+$string['membersadded'] = 'Mitglieder erfolgreich hinzugefügt';
+
+// Delete membership confirmation strings
+$string['delete_membership_confirmation'] = 'Sind Sie sicher, dass Sie die ausgewählten {$a} Mitglieder entfernen möchten?';
+$string['delete_membership_warning'] = 'Warnung: Diese Aktion ist dauerhaft und kann nicht rückgängig gemacht werden.';
+$string['selected_members_list'] = 'Ausgewählt: {$a}';
+
+// CohortEnrolInstancesPartial component strings
+$string['addenrolinstance'] = 'Einschreibeinstanz Hinzufügen';
+$string['cohortenrolinstances'] = 'Einschreibeinstanzen';
+$string['instancescount'] = 'Instanzen';
+$string['loadingenrolinstances'] = 'Einschreibeinstanzen werden geladen...';
+$string['noenrolinstancesfound'] = 'Keine Einschreibeinstanzen Gefunden';
+$string['noenrolinstancesfounddescription'] = 'Keine Einschreibeinstanzen gefunden.';
+$string['enrolinstanceslist'] = 'Einschreibeinstanzen';
+$string['refresh'] = 'Aktualisieren';
+$string['course'] = 'Kurs';
+$string['enrolmethod'] = 'Methode';
+$string['status'] = 'Status';
+$string['active'] = 'Aktiv';
+$string['inactive'] = 'Inaktiv';
+$string['cohortenrol'] = 'Kohorte';
+$string['view'] = 'Ansehen';
+$string['deleteenrolinstance'] = 'Einschreibeinstanz löschen';
+$string['deleteenrolinstanceconfirmation'] = 'Sind Sie sicher, dass Sie die Einschreibeinstanz für "%s" löschen möchten?';
+$string['deleteenrolinstancewarning'] = 'Warnung: Dies entfernt {$a} Benutzereinschreibungen aus diesem Kurs.';
+$string['enrolinstancedeletedsuccessfully'] = 'Einschreibeinstanz erfolgreich gelöscht';
+$string['searchenrolinstances'] = 'Einschreibeinstanzen suchen...';
+$string['filterbystatus'] = 'Nach Status filtern';
+$string['all'] = 'Alle';
+$string['togglestatus'] = 'Status umschalten';
+$string['enrolinstancestatusupdated'] = 'Status der Einschreibeinstanz erfolgreich aktualisiert';
+$string['errortogglingstatus'] = 'Fehler beim Aktualisieren des Status der Einschreibeinstanz';
+$string['enrolinstanceupdated'] = 'Einschreibeinstanz erfolgreich aktualisiert';
+
+// Roles management component strings
+$string['rolesmanagement'] = 'Rollenverwaltung';
+$string['usercontextroles'] = 'Rollen im Benutzerkontext';
+$string['searchroles'] = 'Rollen suchen...';
+$string['loadingroles'] = 'Rollen werden geladen...';
+$string['rolename'] = 'Rollenname';
+$string['roleshortname'] = 'Kurzname';
+$string['roleid'] = 'ID';
+$string['rolesortorder'] = 'Reihenfolge';
+$string['rolearchetype'] = 'Archetyp';
+$string['roleactions'] = 'Aktionen';
+$string['editrole'] = 'Bearbeiten';
+$string['deleterole'] = 'Löschen';
+$string['createnewrole'] = 'Neue Rolle Erstellen';
+$string['addnewrole'] = 'Neue Rolle hinzufügen';
+$string['rolecreatedsuccessfully'] = 'Rolle erfolgreich erstellt';
+$string['roleupdatedsuccessfully'] = 'Rolle erfolgreich aktualisiert';
+$string['roledeletedsuccessfully'] = 'Rolle erfolgreich gelöscht';
+$string['rolenotfound'] = 'Rolle nicht gefunden';
+$string['roleexists'] = 'Eine Rolle mit diesem Kurznamen existiert bereits';
+$string['noassignableroles'] = 'Keine zuweisbaren Rollen im Benutzerkontext verfügbar.';
+$string['roledescriptionplaceholder'] = 'Beschreibung eingeben (optional)';
+$string['rolearchetypeplaceholder'] = 'Archetyp auswählen (optional)';
+$string['rolecreate'] = 'Rolle Erstellen';
+$string['roleupdate'] = 'Rolle Aktualisieren';
+$string['roledeleteconfirmation'] = 'Sind Sie sicher, dass Sie "%s" löschen möchten? Diese Aktion kann nicht rückgängig gemacht werden.';
+$string['deletethisrole'] = 'Diese Rolle löschen';
+$string['cannotdeletethisrole'] = 'Diese Rolle kann nicht gelöscht werden, da sie eine Systemrolle ist.';
+$string['cannotdeleterolewithid'] = 'Rolle mit ID {$a} kann nicht gelöscht werden.';
+$string['rolesearchplaceholder'] = 'Nach Name, Kurzname oder benutzerdefiniertem Namen suchen...';
+$string['rolescount'] = 'Rollen';
+$string['norolesfound'] = 'Keine Rollen gefunden';
+$string['norolesfounddesc'] = 'Keine Rollen gefunden.';
+$string['createyourfirstrole'] = 'Erstellen Sie Ihre erste Rolle, um zu beginnen.';
+
+// DynamicCohortMembersTable component strings
+$string['searchmembers'] = 'Mitglieder suchen...';
+$string['first'] = 'Erste';
+$string['last'] = 'Letzte';
+$string['columns'] = 'Spalten';
+$string['city'] = 'Stadt';
+$string['country'] = 'Land';
+$string['showing'] = 'Angezeigt';
+$string['to'] = 'bis';
+$string['previous'] = 'Vorherige';
+$string['next'] = 'Nächste';
+$string['failedtofetchmembers'] = 'Mitglieder konnten nicht abgerufen werden';
+$string['anerroroccurred'] = 'Ein Fehler ist aufgetreten';
+$string['unknown'] = 'Unbekannt';
+$string['errordeletingmembers'] = 'Fehler beim Löschen von Mitgliedern';
+
+// CohortEnrolInstancesSearchModal component strings
+$string['searchcourses'] = 'Kurse suchen';
+$string['searchcoursesplaceholder'] = 'Kurse suchen...';
+$string['searching'] = 'Suche...';
+$string['nocoursesfound'] = 'Keine Kurse gefunden';
+$string['selectedcourses'] = 'Ausgewählte Kurse';
+$string['coursealreadyselected'] = 'Kurs bereits ausgewählt';
+$string['noselectedcourses'] = 'Keine Kurse ausgewählt';
+$string['role'] = 'Rolle';
+$string['group'] = 'Gruppe';
+$string['remove'] = 'Entfernen';
+$string['enroled'] = 'Eingeschrieben';
+$string['groupmembers'] = 'Gruppenmitglieder';
+$string['enrolinstanceadded'] = 'Einschreibeinstanz erfolgreich hinzugefügt';
+$string['errorcreatingenrolinstances'] = 'Fehler beim Erstellen der Einschreibeinstanzen. Bitte versuchen Sie es erneut.';
+
+// Cohort role assignments (tool_cohortroles integration) strings
+$string['cohortroles'] = 'Kohortenrollen';
+$string['cohortroleassignments'] = 'Zuweisungen';
+$string['cohortroleassignmentscount'] = 'Zuweisungen';
+$string['loadingcohortroles'] = 'Rollenzuweisungen werden geladen...';
+$string['noroughassignmentsfound'] = 'Keine Rollenzuweisungen Gefunden';
+$string['noroughassignmentsfounddescription'] = 'Keine Zuweisungen gefunden. Weisen Sie Benutzer mit Rollen zu, um eine Hierarchie zu erstellen.';
+$string['createyourfirstassignment'] = 'Erstellen Sie Ihre erste Zuweisung, um zu beginnen.';
+$string['addroleassignment'] = 'Zuweisung Hinzufügen';
+$string['assigneduser'] = 'Benutzer';
+$string['assignedrole'] = 'Rolle';
+$string['assignedcohort'] = 'Kohorte';
+$string['assignedtime'] = 'Zugewiesen Am';
+$string['deleteassignment'] = 'Zuweisung löschen';
+$string['deleteassignmentconfirmation'] = 'Sind Sie sicher, dass Sie diese Zuweisung löschen möchten? Sie wird bei der nächsten Synchronisation entfernt.';
+$string['cohortroleassignmentcreated'] = 'Zuweisung erstellt. Sie wird nach der nächsten geplanten Synchronisation wirksam.';
+$string['cohortroleassignmentdeleted'] = 'Zuweisung erfolgreich gelöscht.';
+$string['cohortroleassignmentexists'] = 'Diese Zuweisung existiert bereits.';
+$string['backgroundsyncnote'] = 'Zuweisungen werden nach der geplanten Synchronisation wirksam.';
+$string['synclastsync'] = 'Letzte';
+$string['syncnextsync'] = 'Nächste';
+$string['syncnever'] = 'Nie';
+$string['selectuser'] = 'Wählen Sie einen Benutzer';
+$string['searchusers'] = 'Benutzer suchen...';
+$string['selectrole'] = 'Wählen Sie eine Rolle';
+$string['nousersfound'] = 'Keine Benutzer gefunden';
+$string['assignroletouser'] = 'Rolle Zuweisen';
+$string['userrolecohortinfo'] = 'Weist eine Rolle im Benutzerkontext für alle Mitglieder zu.';
+$string['assignedon'] = 'Zugewiesen am';
+$string['deletethisassignment'] = 'Diese Zuweisung löschen';
+$string['assigneduserscount'] = 'Zuweisungen';
+$string['formvalidationerror'] = 'Bitte korrigieren Sie die Fehler im Formular.';
